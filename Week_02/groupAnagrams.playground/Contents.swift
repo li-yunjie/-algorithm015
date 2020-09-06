053e663b48381074491cec46dc019e93de0258a1
li-yunjie <li-yunjie@mercari.com>
2020-09-06 22:13:34 +0900

    group anagrams

--- Week_02/groupAnagrams.playground/Contents.swift
+++ Week_02/groupAnagrams.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var str = "Hello, playground"
+func groupAnagrams(_ strs: [String]) -> [[String]] {
+    if strs.count == 0 || strs.count == 1 { return [] }
+    var map:[String:[String]] = [:]
+    for str in strs {
+        let char = String(str.sorted())
+        if !map.keys.contains(char) {
+            map[char] = [String]()
+        }
+        map[char]!.append(str)
+    }
+    return Array(map.values)
+}
